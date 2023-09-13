b4f721d10d558116a76b197bd08785d33efc6af5
Jose Vicente Ayala Luna <jayala@starbucks.com>
2023-09-13 16:40:41 -0500

    Fix H2 init issue

--- sports/src/main/java/com/reactor/sports/model/Sport.java
+++ sports/src/main/java/com/reactor/sports/model/Sport.java
@@ -1,14 +1,20 @@
 package com.reactor.sports.model;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @Table("sports")
+@Entity
 public class Sport {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
 }
