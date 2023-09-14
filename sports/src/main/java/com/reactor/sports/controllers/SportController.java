3babf28c3da70428db82ccfa757de51151b28975
Jose Vicente Ayala Luna <jayala@starbucks.com>
2023-09-14 12:54:40 -0500

    Improve create sports endpoint

--- sports/src/main/java/com/reactor/sports/controllers/SportController.java
+++ sports/src/main/java/com/reactor/sports/controllers/SportController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -37,9 +38,9 @@ public class SportController {
     }
 
     @PostMapping
-    public Mono<ResponseEntity<Sport>> createSport(@RequestBody Sport sport) {
-        return sportService.createSport(sport)
-                .map(createdSport -> ResponseEntity.status(HttpStatus.CREATED).body(createdSport));
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Sport> createSport(@RequestBody Sport sport) {
+        return sportService.createSport(sport);
     }
 
     @PostMapping("/{sportName}")
